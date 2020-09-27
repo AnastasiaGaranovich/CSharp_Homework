b84cd0f78625948de060c5779eba64a66b63e303
Nastya <nastymadness@gmail.com>
2020-09-27 23:33:26 +0300

    Homework 1 complete tasks 1, 2, 4, 5, 6,7

---
+++ Homework 1/Homework 1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_1
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Enter the temperature value in Fahrenheit: ");
+            double temperatureF = Convert.ToDouble(Console.ReadLine());
+            double temperatureC = (temperatureF - 32) * 5 / 9;
+            Console.WriteLine($"Temperature in Fahrenheit {temperatureF} = {temperatureC} Temperature in Celsius");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Homework 1/Task3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the start number: ");
+            uint numberStart = Convert.ToUInt32(Console.ReadLine());
+            Console.Write("Enter the end number: ");
+            uint numberEnd = Convert.ToUInt32(Console.ReadLine());
+
+            for(uint i = numberStart; i <= numberEnd; i++)
+            {
+                for(uint j = 0; j < i; j++)
+                {
+                    Console.Write($"{i} ");
+                }
+                Console.WriteLine("");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
