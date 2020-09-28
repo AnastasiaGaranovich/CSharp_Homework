ff719dcca484cc82d4fbba8954909d0ae9572891
Nastya <nastymadness@gmail.com>
2020-09-28 17:02:26 +0300

    Complete tasks 3, 8, 9 and rename all tasks

---
+++ Homework 1/Task8/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The perfect numbers 1 and 10000 are: ");
+            int perfectNumber = 1;
+            int currentNumber = 1; 
+            int sum = 0;
+
+            while(perfectNumber <= 10000)
+            {
+                while(currentNumber <= 10000)
+                {
+                    if (currentNumber < perfectNumber)
+                    {
+                        if (perfectNumber % currentNumber == 0)
+                        {
+                            sum += currentNumber;
+                        } 
+                    }
+                    currentNumber++;
+                }
+                if (sum == perfectNumber)
+                {
+                    Console.Write($"{perfectNumber} ");
+                }
+                perfectNumber++;
+                currentNumber = 1;
+                sum = 0;
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Homework 1/Task9/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the rectangle length: ");
+            int length = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter the rectangle width: ");
+            int width = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter a symbol for the outline of the rectangle: ");
+            char symbolOutline = Convert.ToChar(Console.ReadLine());
+            Console.WriteLine("Enter a symbol to fill the rectangle: ");
+            char symbolFill = Convert.ToChar(Console.ReadLine());
+
+            for (int i = 0; i <length; i++)
+            {
+                if(i == 0 || i == length - 1)
+                {
+                    for (int j = 0; j < width; j++)
+                    {
+                        Console.Write($"{symbolOutline}");
+                    }
+                }
+                else
+                {
+                    for (int j = 0; j < width; j++)
+                    {
+                        if(j == 0 || j == width - 1)
+                        {
+                            Console.Write($"{symbolOutline}");
+                        }
+                        else
+                        {
+                            Console.Write($"{symbolFill}");
+                        }
+                    }
+                }
+                Console.WriteLine(" ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
+ 
