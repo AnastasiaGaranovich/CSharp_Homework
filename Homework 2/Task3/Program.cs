9d509e52fb7bbae27cc3caed6d39186440e39e11
AnastasiaGaranovich <nastyamadness@gmail.com>
2020-09-30 13:50:23 +0300

    Complete task3, start task8

---
+++ Homework 2/Task3/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    class Program
+    {
+        static bool IsPolindrom(string word)
+        {
+            int lenght = word.Length;
+            for(int i = 0; i < lenght / 2; i++)
+            {
+                if (word[i] != word[lenght - i - 1])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the word, which you want to check: ");
+            string userWord = Console.ReadLine();
+
+            if(IsPolindrom(userWord))
+            {
+                Console.WriteLine("Word is a polindrom");
+            }
+            else
+            {
+                Console.WriteLine("Word is not a polindrom");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Homework 2/Task8/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter text in russian language: ");
+            string rusText = Console.ReadLine();
+            string[] rusTextArray = rusText.Split(' ');
+            int countWord = 0;
+
+            for(int i = 0; i < rusTextArray.Length; i++)
+            {
+                
+                
+            }
+
+            Console.WriteLine($"The number of words ending with a vowel: {countWord}");
+            Console.ReadKey();
+        }
+    }
+}
