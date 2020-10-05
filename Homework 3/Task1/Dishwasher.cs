038099aa76137be618704fa81e1338bb9f7738c0
AnastasiaGaranovich <nastyamadness@gmail.com>
2020-10-05 20:51:09 +0300

    Add Homework 3, complete task 1

---
+++ Homework 3/Task1/Dishwasher.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    internal class Dishwasher
+    {
+        private string _model;
+        private double _operatingTime;
+        private double _waterConsumption;
+        private double _powerConsumption;
+        private int _waterTemperature;
+        private int _maxLoading;
+        private double _useCount;
+        private static int _warranty;
+        private static string _serviceCenterAddress;
+        
+        public Dishwasher()
+        {
+
+        }
+
+        public Dishwasher(string model)
+        {
+            _model = model;
+        }
+
+        public Dishwasher(string model, int maxLoading)
+        {
+            _model = model;
+            _maxLoading = maxLoading;
+        }
+
+        public Dishwasher(string model, double waterConsumption, double powerConsumption)
+        {
+            _model = model;
+            _waterConsumption = waterConsumption;
+            _powerConsumption = powerConsumption;
+        }
+
+        static Dishwasher()
+        {
+            _warranty = 24;
+            _serviceCenterAddress = "Minsk city Lenin street building 10";
+        }
+
+        public void EcoMode()
+        {
+            _powerConsumption += 0.70;
+            _waterConsumption += 6.55;
+            _operatingTime = 40.0;
+            _waterTemperature = _waterConsumption * _operatingTime / 4;
+        }
+
+        public void EveryDayMode()
+        {
+
+        }
+
+        public void FastMode(int operatingTime)
+        {
+
+        }
+
+
+
+
+
+    }
+}
---
+++ Homework 3/Task1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dishwasher[] dishwashers = new Dishwasher[5];
+            dishwashers[0] = new Dishwasher("Bosch");
+            dishwashers[1] = new Dishwasher("MAUNFELD");
+            dishwashers[2] = new Dishwasher("Samsung", 8);
+            dishwashers[3] = new Dishwasher("Samsung", 12);
+            dishwashers[4] = new Dishwasher("Bosch", 10.2, 0.99);
+
+            double operatingTimeForDry = 15.5;
+            double operatingTimeForFast = 25.8;
+
+            dishwashers[0].EcoMode();
+            dishwashers[2].DryingMode(ref operatingTimeForDry);
+            dishwashers[4].EveryDayMode();
+            dishwashers[3].FastMode(ref operatingTimeForFast);
+
+            Console.WriteLine(dishwashers[2].MaxLoading);
+        }
+    }
+}
