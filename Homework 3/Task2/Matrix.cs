9fd0b04887fe09c31762f8964018dc2fa1606bc8
AnastasiaGaranovich <nastyamadness@gmail.com>
2020-10-07 12:49:15 +0300

    add some methods and properties at class Matrix

--- Homework 3/Task2/Matrix.cs
+++ Homework 3/Task2/Matrix.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32.SafeHandles;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,8 +11,50 @@ namespace Task2
     {
         static Random _rnd = new Random();
         private int _columnSize;
+        public int Column => _columnSize;
+
         private int _lineSize;
+        public int Line => _lineSize;
+
         private int[ , ] _matrix;
+
+        public int Max
+        {
+            get
+            {
+                int maxElement = int.MinValue;
+                for (int i = 0; i < _columnSize; i++)
+                {
+                    for (int j = 0; j < _lineSize; j++)
+                    {
+                        if (_matrix[i, j] > maxElement)
+                        {
+                            maxElement = _matrix[i, j];
+                        }
+                    }
+                }
+                return maxElement;
+            }
+        }
+
+        public int Min
+        {
+            get
+            {
+                int minElement = int.MaxValue;
+                for (int i = 0; i < _columnSize; i++)
+                {
+                    for (int j = 0; j < _lineSize; j++)
+                    {
+                        if (_matrix[i, j] > minElement)
+                        {
+                            minElement = _matrix[i, j];
+                        }
+                    }
+                }
+                return minElement;
+            }
+        }
 
         public Matrix(int columnSize, int lineSize)
         {
@@ -51,6 +94,61 @@ namespace Task2
             }
         }
 
+        public static Matrix MakeCopy(Matrix originalMatrix, int column, int line)
+        {
+            int i, j;
+            Matrix matrixCopy = new Matrix(column, line);
+            for (i = 0; i < originalMatrix.Column; i++)
+            {
+                for (j = 0; j < originalMatrix.Line; j++)
+                {
+                    matrixCopy[i, j] = originalMatrix[i, j];
+                }
+            }
+            return matrixCopy;
+        }
 
+        public int this[int i, int j]
+        {
+            get
+            {
+                return _matrix[i, j];
+            }
+            set
+            {
+                _matrix[i, j] = value;
+            }
+        }
+
+        public static Matrix operator *(Matrix firstMatrix, Matrix secondMatrix)
+        {
+            if(firstMatrix.Column != secondMatrix.Column && firstMatrix.Line != secondMatrix.Line)
+            {
+                throw new ArgumentException("Matrix sizes do not match");
+            }
+            for(int i = 0; i < firstMatrix.Column; i++)
+            {
+                for(int j = 0; j < firstMatrix.Line; j++)
+                {
+                    firstMatrix[i, j] *= secondMatrix[i, j];
+                }
+            }
+            return firstMatrix;
+        }
+
+        public Matrix Transpose(Matrix matrix)
+        {
+            int temporary;
+            for (int i = 0; i < matrix.Column; ++i)
+            {
+                for (int j = i; j < matrix.Line; ++j)
+                {
+                    temporary = matrix[i, j];
+                    matrix[i, j] = matrix[j, i];
+                    matrix[j, i] = temporary;
+                }
+            }
+            return matrix;
+        }
     }
 }
