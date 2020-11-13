a500b9280654d90e74adcfb49930289070a87690
AnastasiaGaranovich <nastyamadness@gmail.com>
2020-11-13 21:13:47 +0300

    Complete task 1 in Homework 11

---
+++ Homework 11/Homework 11/ExchangeRates.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Task_1
+{
+    internal class ExchangeRates
+    {
+        private string _url;
+        private XDocument _document;
+        private XElement _root;
+
+        public ExchangeRates()
+        {
+            _url = @"http://www.nbrb.by/Services/XmlExRates.aspx";
+            _document = XDocument.Load(_url);
+            _root = _document.Root;
+        }
+
+        public void FindExchangeRate(string charCode)
+        {
+            Console.WriteLine($"Date: {_root.Attribute("Date").Value}");
+
+            foreach (var element in _root.Elements("Currency"))
+            {
+
+                if(charCode == element.Element("CharCode").Value)
+                {
+                    Console.WriteLine($"{element.Element("Scale").Value} " + 
+                                      $" {element.Element("CharCode").Value} = " + 
+                                      $"{element.Element("Rate").Value} BYN");
+                    return;
+                }
+            }
+            Console.WriteLine("There is no such currency");
+
+        }
+    }
+}
---
+++ Homework 11/Homework 11/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ExchangeRates rates = new ExchangeRates();
+            Console.Write("Enter currency char code: ");
+            string charCode = Console.ReadLine();
+            rates.FindExchangeRate(charCode);
+
+            Console.ReadKey();
+        }
+    }
+}
